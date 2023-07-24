4caab2c828036914bb0946003dde30bc67311f2b
akshay <chavanakshaykumar10@gmail.com>
2023-07-24 13:18:56 +0530

    dashboard page changes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,48 +1,44 @@
 module Admin
   class UsersController < Admin::ApplicationController
-    # Overwrite any of the RESTful controller actions to implement custom behavior
-    # For example, you may want to send an email after a foo is updated.
-    #
-    # def update
-    #   super
-    #   send_foo_updated_email(requested_resource)
-    # end
-
-    # Override this method to specify custom lookup behavior.
-    # This will be used to set the resource for the `show`, `edit`, and `update`
-    # actions.
-    #
-    # def find_resource(param)
-    #   Foo.find_by!(slug: param)
-    # end
-
-    # The result of this lookup will be available as `requested_resource`
-
-    # Override this if you have certain roles that require a subset
-    # this will be used to set the records shown on the `index` action.
-    #
-    # def scoped_resource
-    #   if current_user.super_admin?
-    #     resource_class
-    #   else
-    #     resource_class.with_less_stuff
-    #   end
-    # end
-
-    # Override `resource_params` if you want to transform the submitted
-    # data before it's persisted. For example, the following would turn all
-    # empty values into nil values. It uses other APIs such as `resource_class`
-    # and `dashboard`:
-    #
-    # def resource_params
-    #   params.require(resource_class.model_name.param_key).
-    #     permit(dashboard.permitted_attributes).
-    #     transform_values { |value| value == "" ? nil : value }
-    # end
+    
     def resource_params
-      params.require(resource_name).permit(:email, :password, :password_confirmation)
+      params.require(resource_name).permit(
+        :blood_group,
+        :contact_no,
+        :date_of_birth,
+        :date_of_joining,
+        :email,
+        :emergency_contact,
+        :employee_id,
+        :employee_type,
+        :password,
+        :esic_no,
+        :father_name,
+        :full_name,
+        :job_type,
+        :linkedin_profile,
+        :location,
+        :login_token,
+        :manager_id,
+        :managers,
+        :marital_status,
+        :mother_name,
+        :name,
+        :notice_period,
+        :official_email,
+        :personal_email,
+        :relieving_date,
+        :remember_created_at,
+        :reset_password_sent_at,
+        :reset_password_token,
+        :resignation_date,
+        :resignation_status,
+        :retention_bonus,
+        :retention_bonus_no,
+        :retention_time,
+        :uan_no,
+        role_ids: [] 
+      )
     end
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
   end
 end
